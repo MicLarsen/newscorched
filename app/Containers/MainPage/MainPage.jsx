c146a79d073886bf3938a1b2323327ffbe077b95
Michael <miclar.deity@gmail.com>
2018-09-04 00:03:27 +0200

    first push

---
+++ app/Containers/MainPage/MainPage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class MainPage extends Component {
+    constructor(prpos) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }    
+}
